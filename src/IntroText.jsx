26bc604cb12ff2f5db819073606dcdd4f2718bc8
Cain Burt <cain.m.burt@gmail.com>
2024-01-04 22:53:22 +0000

    landing page ammends

---
+++ src/IntroText.jsx
@@ -0,0 +1,10 @@
+export default function IntroText()
+{
+    return <div className="intro-wrapper">
+        <h1 className="intro-title" ><span className="outline">Hey, I'm </span>Cain Burt</h1>
+        <div className="intro-role" >I'm a software developer</div>
+        {/* <div className="intro-links" ></div> */}
+
+    </div>
+        
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,20 @@
 import './style.css'
 import { createRoot } from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import IntroText from './IntroText'
+import Background from './Background'
 
 const root = createRoot(document.querySelector('#root'))
 
 root.render(
-    <h1>Hello World</h1>
+    <>  
+        <div className="canvas">
+            <Canvas orthographic camera={{ zoom: 100, position: [0, 0, 100] }}>
+                <Background />
+            </Canvas>
+        </div>
+       
+        <IntroText />
+    </>
+    
 )
