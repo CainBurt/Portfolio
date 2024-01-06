2f092947ba7e2d6e5f0913da8a6069c85413ae58
Cain Burt <cain@thisiscrowd.com>
2024-01-06 18:19:33 +0000

    improved background balls

---
+++ src/Pointer.jsx
@@ -0,0 +1,19 @@
+import * as THREE from 'three'
+import { useRef } from 'react'
+import { BallCollider, RigidBody } from '@react-three/rapier'
+import { useFrame } from '@react-three/fiber';
+
+export default function Pointer({ vec = new THREE.Vector3() })
+{
+    const pointer = useRef()
+
+    useFrame(({ mouse, viewport }) => {
+        pointer.current?.setNextKinematicTranslation(vec.set((mouse.x * viewport.width) / 2, (mouse.y * viewport.height) / 2, 0))
+    })
+
+    return (
+      <RigidBody position={[0, 0, 0]} type="kinematicPosition" colliders={false} ref={pointer} >
+        <BallCollider args={[0.25]} />
+      </RigidBody>
+    )
+}
