340d676893e4aba486818b1c7663bd8533eb79aa
Cain Burt <cain@thisiscrowd.com>
2024-01-17 17:23:54 +0000

    added bubbles

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,37 @@
 import './style.scss'
 import { createRoot } from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame } from "@react-three/fiber"
 import { Suspense } from 'react'
 import { CuboidCollider, BallCollider, Physics, RigidBody } from '@react-three/rapier'
 import IntroText from './IntroText.jsx'
 import Background from './Background.jsx'
 import Pointer from './Pointer.jsx'
 import Navbar from './Navbar.jsx'
+import BubbleBackground from './BubbleBackground.jsx'
+
+
+
 
 const root = createRoot(document.querySelector('#root'))
 
+
 root.render(
-    <>  
-    
+    <>
+
         <div className="canvas">
-            <Canvas orthographic camera={{ zoom: 100, position: [0, 0, 100] }}>
+            <Canvas
+            // orthographic camera={{ zoom: 100, position: [0, 0, 100] }}
+            >
                 <Suspense>
-                    <Physics 
-                        gravity={[0, 0, 0]} 
+                    <Physics
+                        gravity={[0, 0, 0]}
                     >
                         <Pointer />
-                        <Background />
+                        {/* <Background /> */}
+                        <BubbleBackground />
                     </Physics>
+                    {/* <IntroText position={[0, 0, -10]} /> */}
+
                 </Suspense>
             </Canvas>
         </div>
@@ -30,7 +40,8 @@ root.render(
 
         <IntroText />
 
-        
+
+
     </>
-    
+
 )
