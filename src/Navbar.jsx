f5a6c211000873b5510a23d6d85b381e51cbdd17
Cain Burt <cain.m.burt@gmail.com>
2024-02-16 10:30:22 +0000

    fix darkmode colours

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,41 +1,50 @@
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMoon } from '@fortawesome/free-solid-svg-icons'
+import { faMoon, faSun, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons'
 
-
 export default function Navbar() {
-    let isLightMode = true;
+    const [isLightMode, setIsLightMode] = useState(true);
+
     const setDarkMode = () => {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
-        isLightMode = false;
+        setIsLightMode(false);
     }
+
     const setLightMode = () => {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
-        isLightMode = true;
+        setIsLightMode(true);
     }
-    
-    return <header>
-        <div className="header-content">
-            <div className="left-part" style={{display: "flex", transform: "none"}}>
-                <a href="https://www.linkedin.com/in/cain-burt/" target="_BLANK" >
-                    <FontAwesomeIcon icon={ faLinkedinIn } size="2x" />
-                </a>
-                <a href="https://github.com/CainBurt" target="_BLANK" >
-                    <FontAwesomeIcon icon={ faGithub } size="2x" />
-                </a>
+
+    return (
+        <header>
+            <div className="header-content">
+                <div className="left-part" style={{display: "flex", transform: "none"}}>
+                    <a href="https://www.linkedin.com/in/cain-burt/" target="_BLANK" className="icon-wrapper">
+                        <FontAwesomeIcon icon={ faLinkedinIn } size="2x" />
+                    </a>
+                    <a href="https://github.com/CainBurt" target="_BLANK" className="icon-wrapper">
+                        <FontAwesomeIcon icon={ faGithub } size="2x" />
+                    </a>
+                    <a href="mailto:cain.m.burt@gmail.com" target="_BLANK" className="icon-wrapper">
+                        <FontAwesomeIcon icon={ faEnvelope } size="2x" />
+                    </a>
+                </div>
+                
+                <div className="right-part" style={{display: "flex", transform: "none"}}>
+                    <ul>
+                        <li className="icon-wrapper" style={{display: "block", transform: "none"}}>
+                            { isLightMode ? 
+                                <FontAwesomeIcon icon={faMoon} size="2x" onClick={setDarkMode} />
+                                :
+                                <FontAwesomeIcon icon={faSun} size="2x" onClick={setLightMode} />
+                            }
+                        </li>
+                    </ul>
+                </div>
             </div>
-            
-            <div className="right-part" style={{display: "flex", transform: "none"}}>
-                <ul>
-                    <li className="theme-switch" style={{display: "block", transform: "none",}}>
-                        <FontAwesomeIcon icon={faMoon} size="2x" 
-                            onClick={() => (isLightMode ? setDarkMode() : setLightMode())}
-                        />
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </header>
+        </header>
+    );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -9,21 +9,19 @@ import Pointer from './Pointer.jsx'
 import Navbar from './Navbar.jsx'
 import BubbleBackground from './BubbleBackground.jsx'
 import Projects from './components/Projects.jsx'
-
-
-
+import { personal } from './lib/data.jsx';
+import { professional } from './lib/data.jsx';
 
 const root = createRoot(document.querySelector('#root'))
-
 
 root.render(
     <>
         <section className="hero">
             <svg className="background" preserveAspectRatio="xMidYMid slice" viewBox="10 10 80 80">
-                <path fill="#89379D" stroke="#243763" stroke-width="0.2" className="out-top" d="M37-5C25.1-14.7,5.7-19.1-9.2-10-28.5,1.8-32.7,31.1-19.8,49c15.5,21.5,52.6,22,67.2,2.3C59.4,35,53.7,8.5,37-5Z"/>
-                <path fill="#EB690F" stroke="#243763" stroke-width="0.2" className="in-top" d="M20.6,4.1C11.6,1.5-1.9,2.5-8,11.2-16.3,23.1-8.2,45.6,7.4,50S42.1,38.9,41,24.5C40.2,14.1,29.4,6.6,20.6,4.1Z"/>
-                <path fill="#ECC11B" stroke="#243763" stroke-width="0.2" className="out-bottom" d="M105.9,48.6c-12.4-8.2-29.3-4.8-39.4.8-23.4,12.8-37.7,51.9-19.1,74.1s63.9,15.3,76-5.6c7.6-13.3,1.8-31.1-2.3-43.8C117.6,63.3,114.7,54.3,105.9,48.6Z"/>
-                <path fill="#f6ce91" stroke="#243763" stroke-width="0.2" className="in-bottom" d="M102,67.1c-9.6-6.1-22-3.1-29.5,2-15.4,10.7-19.6,37.5-7.6,47.8s35.9,3.9,44.5-12.5C115.5,92.6,113.9,74.6,102,67.1Z"/>
+                <path stroke-width="0.2" className="out-top" d="M37-5C25.1-14.7,5.7-19.1-9.2-10-28.5,1.8-32.7,31.1-19.8,49c15.5,21.5,52.6,22,67.2,2.3C59.4,35,53.7,8.5,37-5Z"/>
+                <path stroke-width="0.2" className="in-top" d="M20.6,4.1C11.6,1.5-1.9,2.5-8,11.2-16.3,23.1-8.2,45.6,7.4,50S42.1,38.9,41,24.5C40.2,14.1,29.4,6.6,20.6,4.1Z"/>
+                <path stroke-width="0.2" className="out-bottom" d="M105.9,48.6c-12.4-8.2-29.3-4.8-39.4.8-23.4,12.8-37.7,51.9-19.1,74.1s63.9,15.3,76-5.6c7.6-13.3,1.8-31.1-2.3-43.8C117.6,63.3,114.7,54.3,105.9,48.6Z"/>
+                <path stroke-width="0.2" className="in-bottom" d="M102,67.1c-9.6-6.1-22-3.1-29.5,2-15.4,10.7-19.6,37.5-7.6,47.8s35.9,3.9,44.5-12.5C115.5,92.6,113.9,74.6,102,67.1Z"/>
             </svg>
             {/* <div className="canvas">
                 <Canvas
@@ -46,8 +44,8 @@ root.render(
         </section>
         <Navbar />
         <div className="main">
-            <Projects title="Some of my Personal Projects"/>
-            {/* <Projects title="Some of my Profesional Projects"/> */}
+            <Projects title="Some of my Professional Projects" data={ professional }/>
+            <Projects title="Some of my Personal Projects" data={ personal }/>
         </div>
 
 
